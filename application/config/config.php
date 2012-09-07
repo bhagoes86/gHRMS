306a21bdfd6d938ef549ff1e4435f1b3b52e9a1d
Gopang Dot <gopang@gopang-HP-Pavilion-dv3-Notebook-PC.(none)>
2012-09-07 08:17:47 +0700

    initialize

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://dev.hrms/';
+$config['base_url']	= 'http://hrms.phpfogapp.com/';
 
 /*
 |--------------------------------------------------------------------------
