e1899c74134c770c78f2c6344081780e424ee872
Gopang <gopang@gopang-tengtop.(none)>
2012-09-25 23:43:00 +0700

    config hrmsdev

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://hrms.phpfogapp.com/';
+$config['base_url']	= 'http://hrmsdev.phpfogapp.com/';
 
 /*
 |--------------------------------------------------------------------------
